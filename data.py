c7623be3ba801eeb14832ff5f942f367a08b8149
nathalie.willems <nathalie.willems@stryker.com>
2023-06-07 13:50:39 +0200

    multihead attention

--- data.py
+++ data.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 from torch.nn import functional as F 
 
 data_file =  "./input.txt"
+n_embd = 32
 batch_size = 4
 block_size = 8 
 max_iters = 5000
@@ -45,22 +46,21 @@ def get_batch(split: str):
     y = torch.stack([data[i+1:i+block_size+1] for i in idx])
     return x, y
 
-text = read_data()
-CHARS = sorted(list(set(text)))
-vocab_size = len(CHARS)
-TRAIN_DATA, VAL_DATA = train_val_split(text)
+@torch.no_grad()
+def estimate_loss(model):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
-#%%
-batch_size = 4 # paralell comp
-block_size = 8 # context for predictions
-
-xb, yb = get_batch("train")
-yb.shape
-
-#%%
 # baseline model == bigram model
-n_embd = 32
-
 class Head(nn.Module):
     def __init__(self, head_size) -> None:
         super().__init__()
@@ -80,6 +80,35 @@ class Head(nn.Module):
         out = weights @ v # (B, T, T) @ (B, T, C) --> (B, T, C)
         return out
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size) -> None:
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size=head_size) for _ in range(num_heads)])
+    
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1) #concatenate along, dim=-1 channel dim 
+
+class FeedForward(nn.Module):
+    def __init__(self, n_embd) -> None:
+        super().__init__()
+        self.feed = nn.Sequential(nn.Linear(n_embd, n_embd), nn.ReLU())
+
+    
+    def forward(self, x):
+        return self.feed(x)
+
+class Block(nn.Module):
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(num_heads=n_head, head_size=head_size)
+        self.fforw = FeedForward(n_embd=n_embd)
+    
+    def forward(self, x):
+        x = self.sa(x)
+        x = self.fforw(x)
+        return x
+
 
 class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size) -> None:
@@ -87,29 +116,37 @@ class BigramLanguageModel(nn.Module):
         # each token reads off the logits for the next token from the embedding table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(head_size=n_embd) # same as C
+        self.blocks = nn.Sequential(Block(n_embd=n_embd, n_head=4), Block(n_embd=n_embd, n_head=4), Block(n_embd=n_embd, n_head=4))
+        # self.sa_head = Head(head_size=n_embd) # same as C
+        self.sa_head = MultiHeadAttention(num_heads = 4, head_size=n_embd//4) # same as C - 4 heads of 8 outputs == 32
+        self.fforw = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets = None):
         B, T = idx.shape
         # idx and targets are both tensors of B, T
         # returns score for next token of size B, T, C (where C == vocab size)
+        idx = idx.to(device)
         token_embeddings = self.token_embedding_table(idx) #(B, T, C) where C == n_embd
-        position_embeddings = self.position_embedding_table(torch.arange(T, device=device) #(T, C)
+        position_embeddings = self.position_embedding_table(torch.arange(T, device=device)) #(T, C)
         x = token_embeddings + position_embeddings # will broadcast position_embeddings from T, C to 1, T, C so it can sum
-        x = self.sa_head(x)
+        # x = self.sa_head(x)
+        # x = self.fforw(x) # B, T, C
+        x = self.blocks(x)
         logits = self.lm_head(x) #(B, T, C) where C == vocab size
         if targets is None:
             loss = None
         else:
             B, T, C = logits.shape
             logits = logits.view(B*T, C)
+            targets = targets.to(device)
             targets = targets.view(B*T)
             loss = F.cross_entropy(logits, targets)
         return logits, loss
     
     def generate(self, idx, max_new_tokens):
         # idx = B x T x C
+        idx = idx.to(device)
         for _ in range(max_new_tokens):
             idx_cond = idx[:, -block_size:]
             logits, loss = self(idx_cond)
@@ -120,22 +157,37 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat([idx, idx_next], dim=1) # B X T C
         return idx
 
-m = BigramLanguageModel(vocab_size=vocab_size)
+
+text = read_data()
+CHARS = sorted(list(set(text)))
+vocab_size = len(CHARS)
+TRAIN_DATA, VAL_DATA = train_val_split(text)
+xb, yb = get_batch("train")
+model = BigramLanguageModel(vocab_size=vocab_size)
+m = model.to(device)
+
+#%%
+# logits, loss = m(idx=xb.to(device), targets=yb.to(device))
 logits, loss = m(idx=xb, targets=yb)
 inp = torch.zeros([1,1], dtype=torch.long)
 print(text_decoder(m.generate(idx=inp, max_new_tokens=100)[0].tolist()))
 # %%
+
 # simple training loop
 optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
-
+eval_interval = 10
+max_iters = 20
 for steps in range(max_iters):
+    if steps % eval_interval == 0 or steps == max_iters - 1:
+        losses = estimate_loss(m)
+        print(f"step {steps}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
     xb, yb = get_batch("train")
     logits, loss = m(idx=xb, targets=yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
 
-print(loss.item())
+#%%
 
 #%%
 #self-attention trick
@@ -147,8 +199,6 @@ for b in range(B):
     for t in range(T):
         cur_x = x[b, :t+1] #t, C
         xbow[b, t] = torch.mean(cur_x, 0)
-
-#%%
 # matrix implementation
 wei = torch.tril(torch.ones(T, T)) #atm weights are just 1, i.e all historical tokens are weighted the same
 wei = wei/wei.sum(1, keepdim=True) # normalise to sum to 1
